1f65417e12e51e6ad224ed4a21f72bfb4d53a379
David Matejka <matej21@matej21.cz>
2018-03-14 18:34:13 +0100

    cs fix

--- src/Bridges/Infrastructure/LogTesterExtension.php
+++ src/Bridges/Infrastructure/LogTesterExtension.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 namespace Mangoweb\Tester\LogTester\Bridges\Infrastructure;
 
@@ -9,21 +9,21 @@ use Nette\DI\CompilerExtension;
 
 class LogTesterExtension extends CompilerExtension
 {
-    public function loadConfiguration()
-    {
-        $builder = $this->getContainerBuilder();
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
 
-        $builder->addDefinition($this->prefix('logTester'))
-            ->setClass(LogTester::class);
-        $builder->addDefinition($this->prefix('logTesterContainerHook'))
-            ->setClass(LogTesterContainerHook::class)
-            ->addTag(MangoTesterExtension::TAG_HOOK);
-        $builder->addDefinition($this->prefix('logTesterTestCaseListener'))
-            ->setClass(LogTesterTestCaseListener::class);
+		$builder->addDefinition($this->prefix('logTester'))
+			->setClass(LogTester::class);
+		$builder->addDefinition($this->prefix('logTesterContainerHook'))
+			->setClass(LogTesterContainerHook::class)
+			->addTag(MangoTesterExtension::TAG_HOOK);
+		$builder->addDefinition($this->prefix('logTesterTestCaseListener'))
+			->setClass(LogTesterTestCaseListener::class);
 
-        $builder->addDefinition($this->prefix('logger'))
-            ->setClass(TestLogger::class)
-            ->setDynamic()
-            ->addTag(MangoTesterExtension::TAG_REQUIRE);
-    }
+		$builder->addDefinition($this->prefix('logger'))
+			->setClass(TestLogger::class)
+			->setDynamic()
+			->addTag(MangoTesterExtension::TAG_REQUIRE);
+	}
 }
